1beb0a3ae7fcdd3cde4ae56b00595d155e8035ae
AI28 <alexandru.ichim07@gmail.com>
2021-03-18 18:50:02 +0200

    Refactor

--- Tema3/main.py
+++ Tema3/main.py
@@ -1,155 +1,155 @@
-def readMatrix(fileLink):
-    file = open(fileLink, "r")
-    size = int(file.readline())
+import copy
+import sys
 
-    myMatrix = [[] for j in range(size)]
 
-    file.readline()
-    for f in file:
-        line = f.split()
-        value = float(line[0][0:len(line[0]) - 1])
-        i = int(line[1][0:len(line[1]) - 1])
-        j = int(line[2])
-        for item in myMatrix[i]:
-            if item[1] == j:
-                value = value + item[0]
-                myMatrix[i].remove(item)
-        myMatrix[i].append((value, j))
+def read_matrix(file_path):
+    file = open(file_path, "r")
+    matrix_dict = {}
+
+    size = file.readline()
+
+    for line in file:
+
+        if line == "\n":
+            continue
+
+        matrix_elements = line.split()
+        matrix_elements = list(map(lambda x: x.strip(","), matrix_elements))
+
+        value = float(matrix_elements[0])
+        i = int(matrix_elements[1])
+        j = int(matrix_elements[2])
+
+        if i not in matrix_dict.keys():
+            matrix_dict[i] = {}
+        if j not in matrix_dict[i].keys():
+            matrix_dict[i][j] = value
+        else:
+            matrix_dict[i][j] += value
+
     file.close()
-    return myMatrix
-def readTriagonalMatrix(fileLink):
-    file = open(fileLink, "r")
+
+    return matrix_dict
+
+
+def read_triang_matrix(file_path):
+    file = open(file_path, "r")
 
     size = int(file.readline())
     p = int(file.readline())
     q = int(file.readline())
 
     a = [0 for i in range(size)]
-    b = [0 for i in range(size)]
-    c = [0 for i in range(size)]
+    b = [0 for i in range(size - p)]
+    c = [0 for i in range(size - q)]
+
+    size_b = len(b)
+    size_c = len(c)
 
     file.readline()
     for i in range(size):
-        a[i] += float(file.readline())
+        a[i] = float(file.readline())
     file.readline()
-    for i in range(size - 1):
-        b[i] += float(file.readline())
+    for i in range(size_b):
+        b[i] = float(file.readline())
     file.readline()
-    for i in range(size - 1):
-        c[i] += float(file.readline())
+    for i in range(size_c):
+        c[i] = float(file.readline())
+
     file.close()
 
     return p, q, [a, b, c]
 
 
-def adauga(Matrice, i, j, value):
-    for aux in (Matrice[i]):
-        if aux[1] == j:
-            value += aux[0]
-            Matrice[i].remove(aux)
-    Matrice[i].append((value, j))
-    return Matrice
+def addition(sparse_matrix, triang_matrix):
+    a, b, c = triang_matrix
+    dict_matrix = copy.copy(sparse_matrix)
 
+    if len(dict_matrix) != len(a):
+        return [], "Dimensiunile matricilor nu coincid."
 
-def adunare(Matrice, TriagonalABC, TriagonalP, TriagonalQ):
-    size = len(Matrice)
-    for i in range(size):
-        Matrice = adauga(Matrice, i, i, TriagonalABC[0][i])
-        if i + TriagonalQ < size - 1:
-            Matrice = adauga(Matrice, i, i + TriagonalQ, TriagonalABC[1][i])
-        if i - TriagonalP > 0 and i < size - 1:
-            Matrice = adauga(Matrice, i, i - TriagonalP, TriagonalABC[2][i - TriagonalP])
-    return Matrice
-
-
-def getItem(Matrice, i, j):
-    for aux in Matrice[i]:
-        if aux[1] == j: return aux
-    return 0
-
-
-def getValue(Matrice, i, j):
-    for aux in Matrice[i]:
-        if aux[1] == j: return aux[0]
-    return 0
-
-
-# def inmultire(Matrice, TriagonalABC, TriagonalP, TriagonalQ):
-#     size = len(Matrice)
-#     matriceAux = [[] for j in range(size)]
-#     # Pentru fiecare rand
-#     for i in range(size):
-#         myIndex = [i + TriagonalQ, i, i - TriagonalP]
-#         helper = [(i, TriagonalABC[0][i])]
-#         if i - TriagonalP >= 0: helper.append((i - TriagonalP, TriagonalABC[2][i - TriagonalP]))
-#         if i + TriagonalQ < size: helper.append((i + TriagonalQ, TriagonalABC[1][i]))
-#
-#         # Exista ~3 randuri
-#         for j in helper:
-#             value = 0
-#             # Pentru fiecare dintre cele 3
-#             for k in range(len(helper)):
-#                 # daca intra in parametri
-#                 if 0 <= helper[k][0] < size:
-#                     aux = getValue(Matrice, i, myIndex[k])
-#                     # adaugam inmultirea dintre valoarea sa si valoare corespondenta in cealalta matrice
-#                     value += helper[k][1] * aux
-#             matriceAux[i].append((value, j[0]))
-#     return matriceAux
-
-def inmultire2(Matrice, TriagonalABC, TriagonalP, TriagonalQ):
-    #initializez size-ul si o matrice auxiliara
-    size = len(Matrice)
-    matriceAux = [[] for j in range(size)]
-
-    #pentru ficare linie
-    for i in range(1, size - 2):
-        #in matricea tridiagonala avem 3 elemente in ordinea : C(i-1) , A(i) , B(i)
-        for j in range(i - 1, i + 2):
-            valueTriagonal = 0
-
-            #C
-            valueTriagonal += TriagonalABC[2][i - 1] * getValue(Matrice, i - 1, j)
-            #A
-            valueTriagonal += TriagonalABC[0][i] * getValue(Matrice, i, j)
-            #B
-            valueTriagonal += TriagonalABC[1][i] * getValue(Matrice, i + 1, j)
-            matriceAux[i].append((valueTriagonal, j))
-    return matriceAux
-
-def makeMatrix(helperMatrix):
-    size = 2022
-    myMatrix = [[0 for x in range(size)] for j in range(size)]
-    for i in range(size - 1):
-        for j in helperMatrix[i]:
-            helper = j[0]
-            myMatrix[i][j[1]] = helper
-    return myMatrix
-
-
-matriceInitiala = readMatrix("a.txt")
-
-triagonalP, triagonalQ, triagonalMatrix = readTriagonalMatrix("b.txt")
-#triagonalMatrix:
-#[0] => lista cu elementele A cu n elemente
-#[1] => lista cu elementele B cu n-1 elemente
-#[2] => lista cu elementele C cu n-1 elemente
-
-matriceTest = inmultire2(matriceInitiala, triagonalMatrix, triagonalP, triagonalQ)
-
-matriceRezultat = readMatrix("aorib.txt")
-
-
-
-#Verifica daca elementele din matriceTest corespund cu cele din matriceRezultat
-aux = 0
-for i in range(len(matriceTest)):
-    for j in matriceTest[i]:
-        if matriceRezultat[i].__contains__(j):
-            print(f"Ok [{i}][{j[1]}]")
-        #            print(f"GoodValue: {getItem(original, i, j[1])}")
+    for i in range(0, len(a)):
+        if i not in dict_matrix[i].keys():
+            dict_matrix[i][i] = a[i]
         else:
-            print(f"Didn't work [{i}][{j[1]}] with values: {j} and {getItem(matriceRezultat, i, j[1])}")
-            #           print(f"Also original: {getItem(myMatrix, i, j[1])}")
-            aux = aux + 1
-print(f"{aux} errors")
+            dict_matrix[i][i] += a[i]
+
+    for i in range(0, len(b)):
+        if (i + 1) not in dict_matrix[i].keys():
+            dict_matrix[i][i + 1] = b[i]
+        else:
+            dict_matrix[i][i + 1] += b[i]
+
+    for i in range(1, len(c) + 1):
+        if (i - 1) not in dict_matrix[i].keys():
+            dict_matrix[i][(i - 1)] = c[i - 1]
+        else:
+            dict_matrix[i][(i - 1)] += c[i - 1]
+
+    return dict_matrix
+
+
+def matrix_equality(matrix_dict_1, matrix_dict_2, eps):
+    if len(matrix_dict_1) != len(matrix_dict_2):
+        return False
+
+    for i in range(0, len(matrix_dict_1)):
+        for j in range(0, len(matrix_dict_1[i])):
+            if j not in matrix_dict_1[i].keys():
+                continue
+            if abs(matrix_dict_1[i][j] - matrix_dict_2[i][j]) > eps:
+                return True
+
+    return True
+
+
+def matrix_product(matrix_dict, tr_matrix_and_sizes):
+    p, q, triang_matrix = tr_matrix_and_sizes
+    a, b, c = triang_matrix
+    result_matrix = {}
+
+    for i in matrix_dict.keys():
+        for j in range(0, len(a)):
+
+            aux_result = 0
+
+            for k in matrix_dict[i].keys():
+
+                factor = 0
+                #aici practic implementez definitia din pdf. a_ij = a daca e pe diag principala, b daca e deasupra si c daca e sub; 0 in caz contrat
+                if j == k:
+                    factor = a[k]
+                else:
+                    if p == (k - j):
+                        factor = c[j]
+                    elif q == (j - k):
+                        factor = b[k]
+
+                aux_result += matrix_dict[i][k] * factor
+
+            if aux_result != 0:
+                if i not in result_matrix.keys():
+                    result_matrix[i] = {}
+                result_matrix[i][j] = aux_result
+
+    return result_matrix
+
+
+def main(eps):
+    matrix_dict = read_matrix('a.txt')
+    p, q, triang_matrix = read_triang_matrix('b.txt')
+    addition_result = addition(matrix_dict, triang_matrix)
+    addition_dict = read_matrix('aplusb.txt')
+
+    print(matrix_equality(addition_result, addition_dict, eps))
+
+    my_product = matrix_product(matrix_dict, (p, q, triang_matrix))
+    mat_product = read_matrix('aorib.txt')
+    # print(my_product)
+    # print(mat_product)
+    print(matrix_equality(my_product, mat_product, eps))
+
+
+if __name__ == "__main__":
+    epsilon = float(sys.argv[1])
+    main(epsilon)
